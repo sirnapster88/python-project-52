de5f9403213ad857811409f2f03f83e57e5eddfb
root <root@DESKTOP-OS1JJFM.localdomain>
2026-01-30 15:31:56 +0300

    add templates

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
-from . import views
+from .views import IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index)
+    path('', IndexView.as_view(), name='index')
 ]
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
-def index(request):
-    return HttpResponse("Добро пожаловать в Task Manager!")
+class IndexView(TemplateView):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'index.html') 
