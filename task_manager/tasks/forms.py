13b4141c378ddcc26cde2afe62d8e24ccfe8b466
sirnapster88 <sir.napster@yandex.ru>
2026-02-26 11:23:28 +0300

    update task.form

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -18,33 +18,11 @@ class TaskForm(forms.ModelForm):
             'executor': gettext_lazy('Исполнитель'),
             'labels':gettext_lazy('Метка')
         }
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-    #     self.fields['status'].queryset = Status.objects.all()
-    #     self.fields['executor'].queryset = User.objects.all()
-    #     self.fields['labels'].queryset = Label.objects.all()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['status'].queryset = Status.objects.all()
+        self.fields['executor'].queryset = User.objects.all()
+        self.fields['labels'].queryset = Label.objects.all()
 
-    #     self.fields['executor'].empty_label = '---------'
-    #     self.fields['status'].empty_label = '---------'
-
-# class TaskFilterForm(forms.Form):
-#     status = forms.ModelChoiceField(
-#         queryset=Status.objects.all(),
-#         required=False,
-#         label="Статус"
-#     )
-#     executor = forms.ModelChoiceField(
-#         queryset=User.objects.all(),
-#         required=False,
-#         label="Исполнитель"
-#     )
-#     label = forms.ModelChoiceField(
-#         queryset=Label.objects.all(),
-#         required=False,
-#         label="Метка"
-#     )
-#     my_task = forms.BooleanField(
-#         label="Только свои задачи",
-#         required=False
-#     )
-    
+        self.fields['executor'].empty_label = '---------'
+        self.fields['status'].empty_label = '---------'
