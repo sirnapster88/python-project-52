a86e040c587f858d5f455ddaa2fe21eb52d9e32a
sirnapster88 <sir.napster@yandex.ru>
2026-02-26 14:43:50 +0300

    update models

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -1,2 +1,7 @@
+from django.contrib.auth.models import User
 
+def full_name(self):
+    return f"{self.first_name} {self.last_name}"
+
+User.add_to_class('__str__', full_name)
 # Create your models here.
